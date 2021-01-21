be51c23d2255f36481b370a294e23c6b6e77d27b
ValentinaPriore <valentinaprioref@gmail.com>
2021-01-21 16:51:50 -0300

    Create init.js

---
+++ init.js
@@ -0,0 +1,16 @@
+var showSpinner = function(){
+    document.getElementById("spinner-wrapper").style.display = "block";
+  }
+  
+  var hideSpinner = function(){
+    document.getElementById("spinner-wrapper").style.display = "none";
+  }
+  showSpinner();
+  /*
+  document.addEventListener("DOMContentLoaded", function(e){
+    hideSpinner();
+    document.getElementById("main").style.display = "block";
+  });
+  */
+  
+  
