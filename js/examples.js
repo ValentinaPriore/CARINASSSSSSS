9d48e80ad11a1dba26f4040766b0f0a2c85dddce
ValentinaPriore <valentinaprioref@gmail.com>
2021-01-31 21:27:13 -0300

    Update examples.js

--- js/examples.js
+++ js/examples.js
@@ -2,3 +2,9 @@ document.getElementById('demosMenu').addEventListener('change', function(e){
     var dropdown = document.getElementById('demosMenu');
     window.location.href = dropdown.options[dropdown.selectedIndex].getAttribute('id') + '.html';
 });
+
+
+function activarSonido(){
+    document.getElementById("primero").play()
+}
+
