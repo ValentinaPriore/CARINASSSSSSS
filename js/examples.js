f41688e73c934ba182b79b9348b571ee50b226ac
ValentinaPriore <valentinaprioref@gmail.com>
2021-02-03 09:23:43 -0300

    Update examples.js

--- js/examples.js
+++ js/examples.js
@@ -9,3 +9,32 @@ window.addEventListener('load', function () {
     document.getElementById("main").style.visibility = "visible"
   })
 
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
+
+
+  function mute() {
+
+   let audios = document.getElementsByTagName("audio") 
+   let primeraudio = document.getElementById("primero")
+
+
+    if(primeraudio.muted === false){
+        for (let i = 0; i < audios.length; i++) {
+            audios[i].muted = true
+            document.getElementById("sound").src = "img/no-sound.png"
+        }
+    }
+    else{
+        for (let i = 0; i < audios.length; i++) {
+            audios[i].muted = false
+            document.getElementById("sound").src = "img/sound-on.png"
+
+            
+        }
+    }
+
+  
+      
+  }
